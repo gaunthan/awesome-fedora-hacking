1ad62edbc5a965173201b5cd98f30433702b28f0
gaunthan <gaunthanhuang@foxmail.com>
2018-03-29 18:48:08 +0800

    Add script for coding.

---
+++ coding-is-life.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Install development softwares
+
+# Install vscode
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+dnf check-update
+sudo dnf install code -y
+
+# Install SpaceVim
+sudo dnf install vim -y
+curl -sLf https://spacevim.org/install.sh | bash
+
+# Install tilix
+sudo dnf install tilix -y
+
+# Install development tools
+
+# Note: Use `dnf grouplist` to list group
+sudo dnf groupinstall "C Development Tools and Libraries" -y
+sudo dnf groupinstall "Development Tools" -y
+sudo dnf install gcc-c++
