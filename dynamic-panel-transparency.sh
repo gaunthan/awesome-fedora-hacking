d8efb98a9a80efdf05c71f16b4ecca85559d998e
Gaunthan Huang <gauntHanHuang@foxmail.com>
2018-06-29 15:14:05 +0800

    Fix path problem.

--- dynamic-panel-transparency.sh
+++ dynamic-panel-transparency.sh
@@ -20,6 +20,7 @@ else
 fi
 
 # Install extension
+mkdir -p ~/.local/share/gnome-shell/extensions
 cp -rf $repo_name/dynamic-panel-transparency@rockon999.github.io ~/.local/share/gnome-shell/extensions/dynamic-panel-transparency@rockon999.github.io
 
 # Enable extension
